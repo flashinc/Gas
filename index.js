e34cc74a658f1809c33316c038eb74e1843a1e73
Lachlan Jowett <lochyjowett1@gmail.com>
2021-12-18 01:01:22 +1100

    yes

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
 // export { component, tag }
 
-function component(componentName) {
-    return (
-        document.querySelector('[component="' + componentName + '"]')
-    )
-}
+class nitro {
+    constructor(componentName) {
+        this.componentName = componentName
+    }
+    new() {
+        return (
+            document.querySelector('[component="' + this.componentName + '"]').innerHTML
+        )
+    }
+    // I want to be able to change the text of an element with a html attribute e.g: <div text="hello"></div>. so to do that i need to be able to store the value of the attribute, in this case "hello", in a variable.
+    // this is used when you are making a component, and you want to change the text of one of the element, or maybe two. E.G:
+    /*
+    component('button') = `
+        <button> ${tagValue} </button>
+    `
+    */
 
+    // Output:
+    /*
+        <button> hello </button>
+    */
+    tag() {
+        return (
+            document.querySelector(tagName)
+            //document.querySelector('[' + tagName + ']')
 
-// I want to be able to change the text of an element with a html attribute e.g: <div text="hello"></div>. so to do that i need to be able to store the value of the attribute, in this case "hello", in a variable.
-// this is used when you are making a component, and you want to change the text of one of the element, or maybe two. E.G:
-/*
-component('button') = `
-    <button> ${tagValue} </button>
-`
-*/
-
-// Output:
-/*
-    <button> hello </button>
-*/
-function tag(tagName) {
-    return (
-        document.querySelector(tagName)
-        //document.querySelector('[' + tagName + ']')
-
-    )
+        )
+    }
 }
