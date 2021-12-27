fab49f1f4544dd5d35777c56e26e1a20e8bbcc37
Lachlan Jowett <lochyjowett1@gmail.com>
2021-12-27 21:17:02 +1100

    Trying to add props
    TODO: Clean up code and actually make it work

--- index.js
+++ index.js
@@ -9,8 +9,31 @@
 // }
 
 function component(componentName, html) {
-    var el = document.getElementById(componentName)
-    el.innerHTML = html
+    let newHtml;
+    let nHtml
+    this.html = html;
+    var el = document.getElementById(componentName);
+    this.html.html.split('{  }')
+    for (var i = 0; i < this.html.html.length; i++) {
+        if (this.html.html[i] === '{') {
+            var start = i;
+            for (var j = i; j < this.html.html.length; j++) {
+                if (this.html.html[j] === '}') {
+                    var end = j;
+                    var value = this.html.html.slice(start + 1, end);
+                    console.log(value);
+                    // remove all the spaces from value
+                    value = value.replace(/\s/g, '');
+                    var link = document.getElementById(componentName)
+                    var target = link.getAttribute(value);
+                    console.log(target);
+                    nHtml = target;
+                    newHtml = this.html.html.replace('{  }', nHtml);
+                }
+            }
+        }
+    }
+    el.innerHTML = newHtml;
 }
 
 // let link = document.querySelector('#js');
